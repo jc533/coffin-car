831a5ac7c2f15a5e00273f902381036d20ccedc3
justinChen <yub612@gmail.com>
2020-06-10 10:32:22 +0800

    wait func

--- car.ino
+++ car.ino
@@ -4,7 +4,8 @@ long duration, cm;
 const int In1 = 4;
 const int In2 = 5;
 const int In3 = 6;      
-const int In4 = 7;       
+const int In4 = 7;  
+const int wallDistance = 20;
 
 void setup(){
     Serial.begin(9600);       // Serial Port begin
@@ -15,7 +16,6 @@ void setup(){
     pinMode(In3, OUTPUT);
     pinMode(In4, OUTPUT);  
 }
-
 int distance(){
     digitalWrite(trigPin, LOW);
     delayMicroseconds(5);
@@ -26,18 +26,25 @@ int distance(){
     pinMode(echoPin, INPUT);           // 讀取 echo 的電位
     duration = pulseIn(echoPin, HIGH); // 收到高電位時的時間
 
-    cm = (duration / 2) / 29.1; // 將時間換算成距離 cm 或 inch
-    Serial.print("Distance : ")
-    Serial.print(cm)
-    Serial.print("cm")
+    cm = (duration/2)/29.1; // 將時間換算成距離 cm 或 inch
+    Serial.print("Distance : ");
+    Serial.print(cm);
+    Serial.print("cm");
     Serial.println();
     return cm;
     //delay(250);
 }
+bool wait(int waitTime){
+    bool isTime = 0;
+    time = millis();
+    if(time >= waitTime)
+        isTime = 1;
+    return isTime;
+}
 void loop(){
-    distance()
-    delay(250)
-
+    distance();
+    delay(250);
+    time = millis();
     // mfront();
     // delay(2000);
     // mstop();
@@ -46,6 +53,11 @@ void loop(){
     // delay(2000);
     // mstop();
     // delay(500);
+}
+void auto_pilot(){
+    if(distance() < 30){
+        msfront()
+    }
 }
 void mstop(){
     digitalWrite(In1, LOW);
