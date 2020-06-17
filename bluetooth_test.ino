2b84f8fe05568b159cdf16eda456e7763974a571
justinChen <yub612@gmail.com>
2020-06-17 11:33:12 +0800

    servo

--- bluetooth_test.ino
+++ bluetooth_test.ino
@@ -1,11 +1,11 @@
 #include <SoftwareSerial.h> // 引用「軟體序列埠」程式庫
+#include <Servo.h> 
 SoftwareSerial BT(3, 2);    // 設定軟體序列埠(接收腳, 傳送腳)
+Servo myservo;
 
 char command;      // 接收序列埠值的變數
 const byte EA = 6; // 馬達 A 的致能接腳
 const byte IA = 7; // 馬達 A 的正反轉接腳
-const byte EB = 5; // 馬達 B 的致能接腳
-const byte IB = 4; // 馬達 B 的正反轉接腳
 
 // 設定 PWM 輸出值
 const byte speed = 130;
@@ -17,26 +17,27 @@ void stop(){                       // 馬達停止
 void forward(){                           // 馬達轉向：前進
     analogWrite(EA, speed); // 馬達 A 的 PWM 輸出
     digitalWrite(IA, HIGH);
-    
+
 }
 void backward(){                           // 馬達轉向：後退
     analogWrite(EA, speed); // 馬達 A 的 PWM 輸出
     digitalWrite(IA, LOW);
 }
-void turnLeft(){                           // 馬達轉向：左轉
-    analogWrite(EB, speed); // 馬達 A 的 PWM 輸出
-    digitalWrite(IB, LOW);  // 馬達 A 反轉
+void turnLeft(){   
+    myservo.write(90)
+    myservo.write(45)
 }
-void turnRight(){                           // 馬達轉向：右轉
-    analogWrite(EB, speed); // 馬達 B 的 PWM 輸出
-    digitalWrite(IB, HIGH);  // 馬達 B 反轉
+void turnRight(){     
+    myservo.write(90)
+    myservo.write(135)
 }
 
 void setup(){
     BT.begin(9600); // 啟動軟體序列埠
-
+    myservo.attach(9);
+    myservo.write(90);
     pinMode(IA, OUTPUT); // 馬達 A 的致能腳位
-    pinMode(IB, OUTPUT); // 馬達 B 的致能腳位
+    // pinMode(IB, OUTPUT); // 馬達 B 的致能腳位
     stop();              // 先停止馬達
 }
 
